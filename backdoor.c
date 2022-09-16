2b7ea21350c6f901a90c0af9747423c05b6ce01e
A155squatter <bigntnuspy@proton.me>
2022-09-16 21:35:40 +0200

    Fix the oopsie

---
+++ backdoor.c
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+//Figure out why this won't run later
+int backdoor = 1;
+int backdont = 0:
+int lots = 10000000000:
+
+
+for i in range(0, lots):
+	if backdoor == 1:
+		backdont != 0;
+	if backdoor = 0()
+		break
+
+if name == main():
+	main()
+
+int main (void){}
